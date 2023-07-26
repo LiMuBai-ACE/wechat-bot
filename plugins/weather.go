73d240696656957075aa40632eddb02c3dde09d9
lishuaibing <shuaibing.li@baozun.com>
2023-07-26 18:11:56 +0800

    feat: update

--- plugins/weather.go
+++ plugins/weather.go
@@ -20,6 +20,41 @@ type Province struct {
 type City struct {
 	CityName string `json:"citysName"`
 }
+type WeatherInfo struct {
+	Date      string       `json:"date"`
+	Week      string       `json:"week"`
+	Type      string       `json:"type"`
+	Low       string       `json:"low"`
+	High      string       `json:"high"`
+	FengXiang string       `json:"fengxiang"`
+	FengLi    string       `json:"fengli"`
+	Night     NightWeather `json:"night"`
+	Air       AirQuality   `json:"air"`
+	Tip       string       `json:"tip"`
+}
+
+type NightWeather struct {
+	Type      string `json:"type"`
+	FengXiang string `json:"fengxiang"`
+	FengLi    string `json:"fengli"`
+}
+
+type AirQuality struct {
+	AQI      int    `json:"aqi"`
+	AQILevel int    `json:"aqi_level"`
+	AQIName  string `json:"aqi_name"`
+	CO       string `json:"co"`
+	NO2      string `json:"no2"`
+	O3       string `json:"o3"`
+	PM10     string `jsonpm10"`
+	PM2_5    string `json:"pm2.5"`
+	SO2      string `jsonso2"`
+}
+type Weather struct {
+	Success bool        `json:"success"`
+	City    string      `json:"city"`
+	Info    WeatherInfo `json:"info"`
+}
 
 func loadProvinceData() ([]Province, error) {
 	currentDir, _ := os.Getwd()
@@ -67,26 +102,35 @@ func checkLocation(provinces []Province, location string) string {
 	return ""
 }
 
-func getWeather(city string) {
+func getWeather(city string) (string, error) {
 	url := fmt.Sprintf("http://www.lpv4.cn:10000/api/weather/?city=%v", city) // 替换为目标接口的URL
 
 	// 发送GET请求
 	response, err := http.Get(url)
 	if err != nil {
 		fmt.Printf("发送GET请求失败：%s", err)
-		return
+		return "", nil
 	}
 	defer response.Body.Close()
 
 	// 读取响应内容
 	body, err := ioutil.ReadAll(response.Body)
 	if err != nil {
-		fmt.Printf("读取响应内容失败：%s", err)
-		return
+		return "", fmt.Errorf("读取响应内容失败：%s", err)
 	}
+	var weather Weather
+	err = json.Unmarshal([]byte(body), &weather)
+	if err != nil {
+		return "", fmt.Errorf("JSON转换失败：%s", err)
+	}
+	city, date, week, weatherType, lowTemp, highTemp, fengxiang, fengli, nightType, nightFengxiang, nightFengli, tip :=
+		weather.City, weather.Info.Date, weather.Info.Week, weather.Info.Type, weather.Info.Low, weather.Info.High, weather.Info.FengXiang, weather.Info.FengLi, weather.Info.Night.Type, weather.Info.Night.FengXiang, weather.Info.Night.FengLi, weather.Info.Tip
+	// ctx.ReplyText("城市数据未能解析，请稍后重试，或联系管理员")
+	message := fmt.Sprintf("城市：%s\n日期：%s\n星期：%s\n天气：%s\n温度范围：%s ~ %s\n风向：%s\n风力：%s\n夜间气：%s\n夜间风向：%s\n夜间风力：%s\n提示：%s",
+		city, date, week, weatherType, lowTemp, highTemp, fengxiang, fengli, nightType, nightFengxiang, nightFengli, tip)
 
-	// 打印响应内容
-	fmt.Println(string(body))
+	return message, nil
+
 }
 
 // 获取城市天气
@@ -116,7 +160,12 @@ func (weChatPlugin) Weather(ctx *openwechat.MessageContext) {
 			result := checkLocation(provinces, query)
 
 			if result != "" {
-				getWeather(result)
+				msg, err := getWeather(result)
+				if err != nil {
+					ctx.ReplyText(err.Error())
+					return
+				}
+				ctx.ReplyText(msg)
 			} else {
 				ctx.ReplyText(fmt.Sprintf("未找到与位置 '%v' 匹配的地点", msg))
 			}
