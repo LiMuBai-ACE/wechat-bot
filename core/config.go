9de34634cc082d5f7aff058ce930f01a98709040
lishuaibing <shuaibing.li@baozun.com>
2023-07-28 16:16:25 +0800

    feat: 天气组件更新

--- core/config.go
+++ core/config.go
@@ -9,16 +9,27 @@ var SystemConfig systemConfig
 type systemConfig struct {
 	Serve     ServeConfig   `yaml:"serve"`
 	LogConfig log.LogConfig `yaml:"log"`
+	Amap      AmapConfig    `yaml:"amap"`
 }
 
+// 服务配置
 type ServeConfig struct {
-	Host   string   `yaml:"host"`
+	Host string `yaml:"host"`
+	// 静态文件地址
 	Static []string `yaml:"static"`
 }
 
+// 环境
 type mode string
 
 const (
 	Dev  mode = "development"
 	Prod mode = "production"
 )
+
+// 高德地图配置
+type AmapConfig struct {
+	Key string `yaml:"key"`
+	// 静态文件地址
+	WeatherApi string `yaml:"weatherApi"`
+}
--- plugins/weather.go
+++ plugins/weather.go
@@ -8,55 +8,76 @@ import (
 	"os"
 	"path/filepath"
 	"strings"
+	"wechat-bot/core"
 
 	"github.com/eatmoreapple/openwechat"
 )
 
-type Province struct {
-	Cities       []City `json:"citys"`
-	ProvinceName string `json:"provinceName"`
-}
-
 type City struct {
-	CityName string `json:"citysName"`
+	// 城市名称
+	CityName string `json:"cityName"`
+	// 城市编码
+	AdCode string `json:"adCode"`
+	// 城市区号
+	CityCode string `json:"cityCode"`
 }
-type WeatherInfo struct {
-	Date      string       `json:"date"`
-	Week      string       `json:"week"`
-	Type      string       `json:"type"`
-	Low       string       `json:"low"`
-	High      string       `json:"high"`
-	FengXiang string       `json:"fengxiang"`
-	FengLi    string       `json:"fengli"`
-	Night     NightWeather `json:"night"`
-	Air       AirQuality   `json:"air"`
-	Tip       string       `json:"tip"`
+type Forecast struct {
+	Date           string `json:"date"`            // 日期
+	Week           string `json:"week"`            // 星期几
+	DayWeather     string `json:"dayweather"`      // 白天天气状况
+	NightWeather   string `json:"nightweather"`    // 晚上天气状况
+	DayTemp        string `json:"daytemp"`         // 白天温度
+	NightTemp      string `json:"nighttemp"`       // 晚上温度
+	DayWind        string `json:"daywind"`         // 白天风向
+	NightWind      string `json:"nightwind"`       // 晚上风向
+	DayPower       string `json:"daypower"`        // 白天风力
+	NightPower     string `json:"nightpower"`      // 晚上风力
+	DayTempFloat   string `json:"daytemp_float"`   // 白天温度（浮点数）
+	NightTempFloat string `json:"nighttemp_float"` // 晚上温度（浮点数）
 }
 
-type NightWeather struct {
-	Type      string `json:"type"`
-	FengXiang string `json:"fengxiang"`
-	FengLi    string `json:"fengli"`
+type CityForecast struct {
+	City       string     `json:"city"`       // 城市名称
+	Adcode     string     `json:"adcode"`     // 城市编码
+	Province   string     `json:"province"`   // 省份名称
+	ReportTime string     `json:"reporttime"` // 报告时间
+	Casts      []Forecast `json:"casts"`      // 天气预报列表
 }
 
-type AirQuality struct {
-	AQI      int    `json:"aqi"`
-	AQILevel int    `json:"aqi_level"`
-	AQIName  string `json:"aqi_name"`
-	CO       string `json:"co"`
-	NO2      string `json:"no2"`
-	O3       string `json:"o3"`
-	PM10     string `jsonpm10"`
-	PM2_5    string `json:"pm2.5"`
-	SO2      string `jsonso2"`
+type Live struct {
+	Province         string `json:"province"`          // 省份名称
+	City             string `json:"city"`              // 城市名称
+	Adcode           string `json:"adcode"`            // 城市编码
+	Weather          string `json:"weather"`           // 当前天气状况
+	Temperature      string `json:"temperature"`       // 当前温度
+	WindDirection    string `json:"winddirection"`     // 当前风向
+	WindPower        string `json:"windpower"`         // 当前风力
+	Humidity         string `json:"humidity"`          // 当前湿度
+	ReportTime       string `json:"reporttime"`        // 报告时间
+	TemperatureFloat string `json:"temperature_float"` // 当前温度（浮点数）
+	HumidityFloat    string `json:"humidity_float"`    // 当前湿度（浮点数）
 }
+
 type Weather struct {
-	Success bool        `json:"success"`
-	City    string      `json:"city"`
-	Info    WeatherInfo `json:"info"`
+	Status    string         `json:"status"`              // 状态
+	Count     string         `json:"count"`               // 数量
+	Info      string         `json:"info"`                // 信息
+	InfoCode  string         `json:"infocode"`            // 信息代码
+	Forecasts []CityForecast `json:"forecasts,omitempty"` // 预报天气预报列表（可选）
+	Lives     []Live         `json:"lives,omitempty"`     // 实时天气数据列表（可选）
 }
 
-func loadProvinceData() ([]Province, error) {
+// 气象类型
+type Extensions string
+
+const (
+	// base:返回实况天气
+	Base Extensions = "base"
+	// all:返回预报天气
+	All Extensions = "all"
+)
+
+func loadProvinceData() ([]City, error) {
 	currentDir, _ := os.Getwd()
 	// 拼接文件相对路径
 	filePath := filepath.Join(currentDir, "provinces.json")
@@ -65,7 +86,7 @@ func loadProvinceData() ([]Province, error) {
 		return nil, err
 	}
 
-	var provinces []Province
+	var provinces []City
 	err = json.Unmarshal(data, &provinces)
 	if err != nil {
 		return nil, err
@@ -88,49 +109,110 @@ func checkDuplicateStrings(str1, str2 string) bool {
 	return false
 }
 
-func checkLocation(provinces []Province, location string) string {
-	for _, province := range provinces {
-		for _, city := range province.Cities {
-			if checkDuplicateStrings(location, city.CityName) {
-				return city.CityName
-			}
-		}
-		if checkDuplicateStrings(location, province.ProvinceName) {
-			return province.ProvinceName
+func checkLocation(Citys []City, location string) (City, error) {
+	for _, city := range Citys {
+		if checkDuplicateStrings(location, city.CityName) {
+			return city, nil
 		}
 	}
-	return ""
+	return City{}, fmt.Errorf("未找到城市信息！")
 }
 
-func getWeather(city string) (string, error) {
-	url := fmt.Sprintf("http://www.lpv4.cn:10000/api/weather/?city=%v", city) // 替换为目标接口的URL
+// 获取预计天气
+func getWeather(city City, extensions Extensions) (Weather, error) {
+	url := fmt.Sprintf("%v?key=%v&city=%v&extensions=%v", core.SystemConfig.Amap.WeatherApi, core.SystemConfig.Amap.Key, city.AdCode, extensions) // 替换为目标接口的URL
+	var extensionsText = "实时天气"
+	if extensions == All {
+		extensionsText = "预报天气"
+	}
 
 	// 发送GET请求
 	response, err := http.Get(url)
 	if err != nil {
 		fmt.Printf("发送GET请求失败：%s", err)
-		return "", nil
+		return Weather{}, nil
 	}
 	defer response.Body.Close()
-
 	// 读取响应内容
 	body, err := ioutil.ReadAll(response.Body)
 	if err != nil {
-		return "", fmt.Errorf("读取响应内容失败：%s", err)
+		return Weather{}, fmt.Errorf("读取%s响应内容失败：%s", extensionsText, err)
 	}
-	var weather Weather
+
+	weather := Weather{
+		Forecasts: make([]CityForecast, 0),
+		Lives:     make([]Live, 0),
+	}
+
 	err = json.Unmarshal([]byte(body), &weather)
+
 	if err != nil {
-		return "", fmt.Errorf("JSON转换失败：%s", err)
+		return Weather{}, fmt.Errorf("JSON转换失败：%s", err)
 	}
-	city, date, week, weatherType, lowTemp, highTemp, fengxiang, fengli, nightType, nightFengxiang, nightFengli, tip :=
-		weather.City, weather.Info.Date, weather.Info.Week, weather.Info.Type, weather.Info.Low, weather.Info.High, weather.Info.FengXiang, weather.Info.FengLi, weather.Info.Night.Type, weather.Info.Night.FengXiang, weather.Info.Night.FengLi, weather.Info.Tip
-	// ctx.ReplyText("城市数据未能解析，请稍后重试，或联系管理员")
-	message := fmt.Sprintf("城市：%s\n日期：%s\n星期：%s\n天气：%s\n温度范围：%s ~ %s\n风向：%s\n风力：%s\n夜间气：%s\n夜间风向：%s\n夜间风力：%s\n提示：%s",
-		city, date, week, weatherType, lowTemp, highTemp, fengxiang, fengli, nightType, nightFengxiang, nightFengli, tip)
 
-	return message, nil
+	return weather, nil
+}
 
+func getLiveWeatherInfo(live Live) string {
+	info := "当前实时天气信息：\n"
+	info += "省份名称：" + live.Province + "\n"
+	info += "城市名称：" + live.City + "\n"
+	// info += "城市编码：" + live.Adcode + "\n"
+	info += "当前天气状况：" + live.Weather + "\n"
+	info += "当前温度：" + live.Temperature + "℃\n"
+	info += "当前风向：" + live.WindDirection + "\n"
+	info += "当前风力：" + live.WindPower + "\n"
+	info += "当前湿度：" + live.Humidity + "%\n"
+	info += "报告时间：" + live.ReportTime + "\n"
+	return info
+}
+func getForecastWeatherInfo(forecast Forecast) string {
+
+	weekMap := map[string]string{
+		"1": "星期一",
+		"2": "星期二",
+		"3": "星期三",
+		"4": "星期四",
+		"5": "星期五",
+		"6": "星期六",
+		"7": "星期日",
+	}
+
+	info := ""
+	info += "日期：" + forecast.Date + "，" + weekMap[forecast.Week] + "\n"
+	info += "白天天气状况：" + forecast.DayWeather + "\n"
+	info += "晚上天气状况：" + forecast.NightWeather + "\n"
+	info += "白天温度：" + forecast.DayTemp + "\n"
+	info += "晚上温度：" + forecast.NightTemp + "\n"
+	info += "白天风向：" + forecast.DayWind + "\n"
+	info += "晚上风向：" + forecast.NightWind + "\n"
+	info += "白天风力：" + forecast.DayPower + "\n"
+	info += "晚上风力：" + forecast.NightPower + "\n"
+	return info
+}
+
+// 合并天气数据 并进行汇报
+// forecastsWeather 预报天气
+// livesWeather 实时天气
+func processData(forecastsWeather, livesWeather Weather) (string, error) {
+	var liveText = ""
+	for _, live := range livesWeather.Lives {
+		liveText += getLiveWeatherInfo(live)
+	}
+
+	var forecastText = ""
+	for _, cityForecast := range forecastsWeather.Forecasts {
+		forecastText += cityForecast.City + "今天及未来三天的天气预报如下:"
+		for _, forecast := range cityForecast.Casts {
+			forecastText += "\n" + getForecastWeatherInfo(forecast)
+		}
+	}
+
+	if liveText != "" && forecastText != "" {
+		return liveText + "\n" + forecastText, nil
+	}
+
+	return "", fmt.Errorf("合并天气数据失败")
 }
 
 // 获取城市天气
@@ -140,11 +222,11 @@ func (weChatPlugin) Weather(ctx *openwechat.MessageContext) {
 		user, _ := ctx.Sender()
 		group := &openwechat.Group{user}
 		msg := ctx.Content
-		if group.NickName == "中煤集团交流群" && strings.Contains(msg, "天气") {
+		if group.NickName == "中煤集团交流群" && strings.Contains(msg, "天气") && strings.Contains(msg, "查询") {
 
 			provinces, err := loadProvinceData()
 			if err != nil {
-				ctx.ReplyText("城市数据未能解析，请稍后重试，或联系管理员")
+				ctx.ReplyText(fmt.Sprintf("城市数据未能解析，请稍后重试，或联系管理员:%v", err.Error()))
 				return
 			}
 
@@ -157,10 +239,20 @@ func (weChatPlugin) Weather(ctx *openwechat.MessageContext) {
 			// 去除前后空格
 			query = strings.TrimSpace(query)
 
-			result := checkLocation(provinces, query)
+			city, err := checkLocation(provinces, query)
 
-			if result != "" {
-				msg, err := getWeather(result)
+			if err == nil {
+				forecastsWeather, err1 := getWeather(city, All)
+				livesWeather, err2 := getWeather(city, Base)
+				if err1 != nil {
+					ctx.ReplyText(err.Error())
+					return
+				}
+				if err2 != nil {
+					ctx.ReplyText(err.Error())
+					return
+				}
+				msg, err = processData(forecastsWeather, livesWeather)
 				if err != nil {
 					ctx.ReplyText(err.Error())
 					return
